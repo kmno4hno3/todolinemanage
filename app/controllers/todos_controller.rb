9296a24488061faf1618a5f6c05a2648498c3fbc
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2020-08-03 12:04:59 +0900

    delete_complete

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,41 @@
+class TodosController < ApplicationController
+  def index
+    @todos = Todo.all
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update!(todo_params)
+    redirect_to todos_url, notice: "todo「#{todo.name}」を更新しました。"
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    todo = Todo.new(todo_params)
+    todo.save!
+    redirect_to todos_url, notice: "todo「#{todo.name}」を登録しました。"
+  end
+
+  def destroy
+    todo = Todo.find(params[:id])
+    todo.destroy
+    redirect_to todos_url, notice: "Todo「#{todo.name}」を削除しました。"
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks
-
-  root to: 'tasks#index'
+  resources :todos
+  root to: 'todos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
