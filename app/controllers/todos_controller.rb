0e7e0b71c82bee7881ffb67deafae83a2e932ee7
kmno4hno3 <kmno4hno3@ezweb.ne.jp>
2020-08-03 17:58:50 +0900

    chapter4_complete

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,20 +1,19 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
   def index
-    @todos = Todo.all
+    @todos = current_user.todos.order(created_at: :desc)
   end
 
   def edit
-    @todo = Todo.find(params[:id])
   end
 
   def update
-    todo = Todo.find(params[:id])
-    todo.update!(todo_params)
-    redirect_to todos_url, notice: "todo「#{todo.name}」を更新しました。"
+    @todo.update!(todo_params)
+    redirect_to todos_url, notice: "todo「#{@todo.name}」を更新しました。"
   end
 
   def show
-    @todo = Todo.find(params[:id])
   end
 
   def new
@@ -22,20 +21,27 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo = Todo.new(todo_params)
-    todo.save!
-    redirect_to todos_url, notice: "todo「#{todo.name}」を登録しました。"
+    @todo = Todo.new(todo_params.merge(user_id: current_user.id))
+
+    if @todo.save
+      redirect_to @todo, notice: "todo「#{@todo.name}」を登録しました。"
+    else
+      render :new
+    end
   end
 
   def destroy
-    todo = Todo.find(params[:id])
-    todo.destroy
-    redirect_to todos_url, notice: "Todo「#{todo.name}」を削除しました。"
+    @todo.destroy
+    redirect_to todos_url, notice: "Todo「#{@todo.name}」を削除しました。"
   end
 
   private
 
   def todo_params
     params.require(:todo).permit(:name, :description)
+  end
+
+  def set_todo
+    @todo = current_user.todos.find(params[:id])
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,16 @@
 class Todo < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30}
+  validate :validate_name_not_including_comma
+
+  belongs_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  private
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'カンマを含めることはできません') if name&.include?(',')
+  end
+
 end
